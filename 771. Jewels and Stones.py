31082e1ef99e7ca23f00b283bb9a22e7bb009af4
Lipeng Zhu朱力鹏 <lipengzhu96@163.com>
2018-05-06 20:18:55 +0800

    update notes

--- 771. Jewels and Stones.py
+++ 771. Jewels and Stones.py
@@ -4,3 +4,29 @@ def intersect(seq1,seq2):
                 if x in seq2:
                         res.append(x)
         return len(res)
+
+'''
+Best Solutions
+1.
+def numJewelsInStones(self, J, S):
+        return sum(map(J.count, S))
+
+2.
+def numJewelsInStones(self, J, S):
+        return sum(map(S.count, J))
+
+3.
+def numJewelsInStones(self, J, S):
+        return sum(s in J for s in S)
+'''
+
+'''
+Notes
+1. usage of map(function, list): map for specified function according to given list
+2. list.count(c): count the number of occurrences of c in list
+3. sum(s in J for s in S) is equal to:
+        for s in S:
+            if s in J:
+               res += 1
+        ruturn res
+'''
