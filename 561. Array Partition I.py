76defbb3604a3dd98ac6b3b75b082edf448d2eb8
lipengzhu1996 <lipeng_zhu@outlook.com>
2018-05-08 17:04:18 +0800

    update

---
+++ 561. Array Partition I.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(sorted(nums)[::2])
+'''
+Good Job! One of the best solutions!
+'''
