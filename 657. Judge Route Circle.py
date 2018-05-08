2dcb122100f282bdd02d3e60bc2baa01f6dfec70
Lipeng Zhu朱力鹏 <lipengzhu96@163.com>
2018-05-08 15:51:23 +0800

    update

---
+++ 657. Judge Route Circle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        mov = {"U":1, "D":-1, "R":0.5, "L":-0.5}
+        
+        return not bool(sum(mov[i] for i in moves))
+        
+'''
+Best Solutions
+1.
+return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')
+'''
+
