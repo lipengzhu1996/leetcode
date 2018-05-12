faaa7af884d084188a149271c1d0a8d9869cea6b
lipengzhu1996 <lipeng_zhu@outlook.com>
2018-05-12 13:30:06 +0800

    up

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -16,3 +16,20 @@ class Solution(object):
             p = nextnode
             
         return prevnode
+		
+'''
+Recursively:
+
+class Solution(object):
+    def reverseList(self, head, prev = None):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return prev
+        
+        curr = head.next
+        head.next = prev
+        return self.reverseList(curr, head)
+'''
